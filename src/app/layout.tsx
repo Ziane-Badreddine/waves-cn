12f7a71167d18312524cfda901360ed16c58f636
Ziane-Badreddine <zianebadredddine2004@gmail.com>
2026-02-25 21:51:43 +0000

    __metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,19 @@ import { RootProvider } from "fumadocs-ui/provider/next";
 import "./global.css";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip"
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Metadata } from "next";
 
 const inter = Inter({
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | @wave/cn",
+    default: "@wave/cn",
+  },
+};
 
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
